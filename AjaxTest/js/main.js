02ecd5997e55aacf06a75b2a348f42f137c3d5ae
MalinaMan <nick.malinovskiy@gmail.com>
2018-07-30 23:10:23 +0300

    ready

--- AjaxTest/js/main.js
+++ AjaxTest/js/main.js
@@ -2,9 +2,11 @@
   "use strict";
 
   var users;
-  var button = document.getElementById("load");
+  let button = document.getElementById("load");
   button.addEventListener('click', loadUsers);
 
+  document.getElementById('close').addEventListener('click', function() { show('none'); });
+  document.getElementById('sortSelect').addEventListener('change', function() { processData(users); });
 
   function loadUsers() {
     var xhr = new XMLHttpRequest();
@@ -24,7 +26,10 @@
         if ('results' in data) {
           button.innerHTML = 'load';
           button.disabled = false;
-          processData(data.results);
+          users = data.results;
+
+          processData(users);
+          document.querySelector('.table').addEventListener('click', showDetailInfo, false);
         }
         else {
           alert("Format error: no properties name 'results'");
@@ -38,19 +43,55 @@
 
   function getPostsTemplate(users) {
     return users.reduce((tmpl, user) => {
+      let name = user.name;
       tmpl += `<div class="tr" data-index=${users.indexOf(user)}>
           <div class="td users-avatar"><img src=${user.picture.medium}></div>
-          <div class="td users-name">${user.name.title}. ${user.name.first} ${user.name.last}</div>
+          <div class="td users-name">${name.title}. ${name.first} ${name.last}</div>
         </div>`;
       return tmpl;
     }, '');
   }
 
+  function propComparator(prop) {
+    
+    let orderDesc = (prop[0] === '-') ? -1 : 1;
+    prop = ~orderDesc ? prop : prop.substring(1);
+    
+    let pointPosition = prop.indexOf('.');
+    let propBegin = prop;
+    let propEnd = prop;
+    if (~pointPosition) {
+      propBegin = prop.substring(0, pointPosition);
+      propEnd = prop.substring(pointPosition + 1);
+    }
+    
+    return function(a, b) {
+        if (~pointPosition) {
+          a = a[propBegin];
+          b = b[propBegin];
+        }        
+        let res = (a[propEnd] < b[propEnd]) ? -1 : (a[propEnd] > b[propEnd]) ? 1 : 0;
+        return orderDesc * res;
+    }
+  }
+
+  function sortData(users)
+  {
+    let selectElem = document.getElementById('sortSelect');
+    let sortValue = selectElem.options[selectElem.selectedIndex].value;
+    return users.sort(propComparator(sortValue));
+  }
+
   function processData(users)
   {
+    if (!users) {
+      return;
+    }
+
+    users = sortData(users);
+
     let oldChild = document.querySelector('.table');
     let item = document.createElement("div");
-    let newElem;
 
     item.className = 'table';
     item.innerHTML = `<div class="tr">
@@ -58,21 +99,59 @@
       <div class="th users-name">Users name</div>
       </div>` +
     getPostsTemplate(users);
+    
     let mainNode = document.querySelector('.main');
     if (oldChild) {
-      newElem = mainNode.replaceChild(item, oldChild);
+      mainNode.replaceChild(item, oldChild);
     } else {
-      newElem = mainNode.appendChild(item);
+      mainNode.appendChild(item);
     }
-    newElem.addEventListener('click', showDetailInfo, false);
   }
 
   function showDetailInfo(event)
   {
-    //let { parentNode } = event.target;
-    if (event.target.className === 'tr'){
-      alert(parentNode.dataset.index);
+    let parentNode = event.target.parentNode;
+    if (!parentNode || parentNode.className !== 'tr') {
+      return;
     }
+    show('block');
+    fillDetailsForm(parentNode.dataset.index);
+  }
+
+  function show(state)
+  {
+    document.getElementById('popup-window').style.display = state;
+    document.getElementById('wrap').style.display = state;
+  }
+
+  function addChildLi(list, val)
+  {
+    let newLi = document.createElement('li');
+    newLi.innerHTML = val;
+    list.appendChild(newLi);
+  }
+
+  function fillDetailsForm(indexArr)
+  {
+    let user = users[indexArr];
+    let name = user.name;
+
+    document.querySelector('#caption').innerHTML = `<b>${name.title} ${name.first} ${name.last}</b>`;
+    
+    let avatar = document.querySelector('#big-avatar');
+    if (avatar) {
+      avatar.innerHTML = `<img src=${user.picture.large}>`;
+    }
+
+    let detailsList = document.querySelector('#details-list');
+    detailsList.innerHTML = '';
+    
+    let location = user.location;
+    addChildLi(detailsList, `Street: ${location.street}`);
+    addChildLi(detailsList, `City: ${location.city}`);
+    addChildLi(detailsList, `State: ${location.state}`);
+    addChildLi(detailsList, `E-mail: ${user.email}`);
+    addChildLi(detailsList, `Phone: ${user.phone}`);
   }
 
 }());
